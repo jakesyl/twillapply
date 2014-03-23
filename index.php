e257812adfce901ae7ccfd99fde8089f8c7097c2
jakesyl <jakesyl@gmail.com>
2014-03-23 10:21:02 -0400

    mysql connect

--- index.php
+++ index.php
@@ -20,8 +20,8 @@ $message = explode($ui,  "  ");// exploding into location place and body
 $place = $message[0];
 $body =  $message[1];
 
-$con=mysqli_connect("sql4.freesqldatabase.com:3306","sql434134","bY2%tL9%","sql434134");//hi
-if (mysqli_connect_errno())
+$con=mysql_connect("sql4.freesqldatabase.com:3306","sql434134","bY2%tL9%","sql434134");
+if (mysql_connect_errno())
     {
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
@@ -29,20 +29,20 @@ else{
     
 }
 
- $x = mysqli_query("SELECT * FROM Chats WHERE number = '$number'");
-    $row2 = mysqli_fetch_array($x);
+ $x = mysql_query("SELECT * FROM Chats WHERE number = '$number'");
+    $row2 = mysql_fetch_array($x);
     if(count($row2)==0){
-        $result = mysqli_query($con,"INSERT INTO Chats (Numbers, Location) VALUES ('$number', '$place')");
-            $y = mysqli_query("SELECT * FROM Chats WHERE location = '$place'");
-            $row3 = mysqli_fetch_array($x);
+        $result = mysql_query($con,"INSERT INTO Chats (Numbers, Location) VALUES ('$number', '$place')");
+            $y = mysql_query("SELECT * FROM Chats WHERE location = '$place'");
+            $row3 = mysql_fetch_array($x);
             for($x=0; $x<count(row3); $x++){
             $number = $row3[$x]["number"];
             $ournum = +19088180650;
             $sms = $client->account->sms_messages->create($ournum,$number,$body,array());
 }}
     else{
- $y = mysqli_query("SELECT * FROM Chats WHERE location = '$place'");
-            $row3 = mysqli_fetch_array($x);    
+ $y = mysql_query("SELECT * FROM Chats WHERE location = '$place'");
+            $row3 = mysql_fetch_array($x);    
             for($x=0; $x<count(row3); $x++){
             $number = $row3[$x]["number"];
             $ournum = +19088180650;
