ee0cb9973becc822c9db3c00a5b755ffac6d49f6
jakesyl <jakesyl@gmail.com>
2014-03-23 09:14:13 -0400

    fixed resolution commented apexes things

--- index.php
+++ index.php
@@ -1,22 +1,21 @@
-<<<<<<< HEAD
 <?php
 
-$ui = $_POST['Body'];
-$from = $_POST['From'];
+$ui = $_POST['Body'];//Getting the body
+$from = $_POST['From'];//phone number its coming from 
 
 // Get the PHP helper library from twilio.com/docs/php/install
 /require_once('/twilio-php/Services/Twilio.php'); // Loads the library
  
 // Your Account Sid and Auth Token from twilio.com/user/account
-$sid = "AC3b392bc11cfc9762ed23e1488b56c4d2"; 
+$sid = "AC3b392bc11cfc9762ed23e1488b56c4d2"; //tokens
 $token = "1d510d501597d6eec2568ed7be8fa9e5"; 
-$client = new Services_Twilio($sid, $token);
+$client = new Services_Twilio($sid, $token);//random unnecesary things
  
 // Get an object from its sid. If you do not have a sid,
 // check out the list resource examples on this page
 $sms = $client->account->sms_messages->get("SM800f449d0399ed014aae2bcc0cc2f2ec");
 echo $sms->body;
-$message = explode($ui,  "  ");
+$message = explode($ui,  "  ");// exploding into location place and body 
 $place = $message[0];
 $body =  $message[1];
 
@@ -33,12 +32,10 @@ else{
     $row2 = mysqli_fetch_array($x);
     if(count($row2)==0){
         $result = mysqli_query($con,"INSERT INTO Chats (Numbers, Location) VALUES ('$number', '$place')")
-            //this is for sending
+            //this is for sending 
     } 
     else{
         //this is for sending
     }
     
 ?>
-=======
->>>>>>> FETCH_HEAD
