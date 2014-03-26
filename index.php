65a3df86e9fe3e612e43bb078af23980c828702b
jakesyl <jakesyl@gmail.com>
2014-03-25 21:17:00 -0400

    added some comments

--- index.php
+++ index.php
@@ -1,27 +1,27 @@
 <?php
 
-$ui = $_POST['Body'];//Getting the body
-$from = $_POST['From'];//phone number its coming from 
+$ui = $_POST['Body'];//Getting the body via a post request, so it's ssl based
+$from = $_POST['From'];//phone number its coming from let's add this to the database
 
 // Get the PHP helper library from twilio.com/docs/php/install
-require_once('./Services/Twilio.php'); // Loads the library
+require_once('./Services/Twilio.php'); // Loads the library, so Twilio guy told us to do this, wondering why but if it doesn't work we'll try this.... :D
  
 // Your Account Sid and Auth Token from twilio.com/user/account
 $sid = "AC3b392bc11cfc9762ed23e1488b56c4d2"; //tokens
 $token = "1d510d501597d6eec2568ed7be8fa9e5"; 
 $client = new Services_Twilio($sid, $token);//random unnecesary things
-/* 
+ 
 // Get an object from its sid. If you do not have a sid,
 // check out the list resource examples on this page
 $sms = $client->account->sms_messages->get("SM800f449d0399ed014aae2bcc0cc2f2ec");
 echo $sms->body;
-*/
+//he said to block this out, but the problem is its not being used anywhere ^
 $message = explode($ui,  "  ");// exploding into location place and body 
 $place = $message[0];
 $body =  $message[1];
 
 $con=mysql_connect("sql4.freesqldatabase.com:3306","sql434134","bY2%tL9%","sql434134");//used a free sql service because heroku is retarted, use digital ocean in the future
-/*if (mysql_connect_errno())//TODO convert all mysql back to mysqli
+    if (mysql_connect_errno())//TODO convert all mysql back to mysqli
     {
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }//blanked out error cuz it was screwin us up, didn't help...
