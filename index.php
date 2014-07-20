b61c3d021000d10c245ceeca1a00b1d26485a196
aep000 <alexparson1@gmail.com>
2014-07-19 22:31:12 -0400

    shit

--- index.php
+++ index.php
@@ -42,3 +42,14 @@ else{
 ?>
 =======
 >>>>>>> FETCH_HEAD
+
+
+// Get the PHP helper library from twilio.com/docs/php/install
+require_once('/path/to/twilio-php/Services/Twilio.php'); // Loads the library
+ 
+// Your Account Sid and Auth Token from twilio.com/user/account
+$sid = "AC3b392bc11cfc9762ed23e1488b56c4d2"; 
+$token = "{{ auth_token }}"; 
+$client = new Services_Twilio($sid, $token);
+ 
+$client->account->messages->sendMessage("+14158141829", "+1", "NEIN", "http://www.example.com/hearts.png");
